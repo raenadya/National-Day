2468c2615361f5883b77310278d141b14285955f
Rae Nadya <rae.nadya511@gmail.com>
2023-07-22 10:34:20 +0800

    Initial Commit

---
+++ National Day/National_DayApp.swift
@@ -0,0 +1,17 @@
+//
+//  National_DayApp.swift
+//  National Day
+//
+//  Created by Kierae NJ on 22/7/23.
+//
+
+import SwiftUI
+
+@main
+struct National_DayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
