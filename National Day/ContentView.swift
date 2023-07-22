752911ccbbb2f661a81dd068c24bed25e7306c16
Rae Nadya <rae.nadya511@gmail.com>
2023-07-22 12:11:27 +0800

    added 2 typical Singaporean scenarios

--- National Day/ContentView.swift
+++ National Day/ContentView.swift
@@ -1,14 +1,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    @State private var qnOne = ""
+    let qnoneresponse = ["where's your tracetogether token", "right in front of u LA", "turn right turn left turn right turn left", "*ignores*"]
+    @State private var qnTwo = ""
+    let qntworesponse = ["you are a not nice person", "why u so kiasu", "cannot talk to your elders like this"]
+    @State private var qnThree = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Form {
+            
+            Section("Question 1") {
+                Image("woman")
+                    .frame(maxWidth: .infinity, alignment: .center)
+                Picker("If you asked her where the toilet is, what would her response be?", selection: $qnOne) {
+                    ForEach(qnoneresponse, id:\.self) {
+                        Text($0)
+                    }
+                }
+            }
+            
+            Section("Question 2") {
+                Image("mrt uncle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: .infinity, alignment: .center)
+                Picker("If you asked him to stop coughing and put on mask, what would his response be?", selection: $qnTwo) {
+                    ForEach(qntworesponse, id:\.self) {
+                        Text($0)
+                    }
+                }
+            }
+    
+            
         }
-        .padding()
+        
     }
 }
 
@@ -17,3 +45,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
